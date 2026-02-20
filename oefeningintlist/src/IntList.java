cb02ff32b9750affa4f5db1543bd7e4592d833db
Raphael Debruyne <raphael.debruyne@student.kuleuven.be>
2026-02-20 20:14:31 +0100

    Eerste commit bevat nog fouten

---
+++ oefeningintlist/src/IntList.java
@@ -0,0 +1,64 @@
+package intlist;
+
+/**
+ * Elke instantie van deze klasse slaat een reeks getallen op
+ */
+public class IntList {
+	private int[] lijst;
+	
+	public IntList() {
+		this.lijst = new int[0];
+	}
+	/**
+	 * @post | result == getArray().length
+	 */
+	public int getLength() {
+		return lijst.length;
+	}
+	/**
+	 * @pre | index != null
+	 * @pre | 0 <= index && index < getLength()
+	 * @post | result != null
+	 */
+	public int getElementIndex(int index) {
+		return lijst[index];
+	}
+	/**
+	 * @creates | result
+	 * @post | result != null
+	 */
+	public int[] getArray() {
+		return lijst.clone();
+	}
+	/**
+	 * @pre element != null
+	 * @mutates | this
+	 * @post | IntStream.range(0,old(getArray()).clone().length).allMatch(i->getArray()[i]==old(getArray()).clone()[i]) && getArray()[getArray().length-1] == element
+	 */
+	public void addLast(int element) {
+		int lengte = getArray().length;
+		int[] merged = new int[getArray().length+1];
+		for(int i = 0;i < lengte;i++) {
+			merged[i] = getArray()[i];
+		}
+		merged[getArray().length] = element;
+		this.lijst = merged;
+				
+	}
+	/**
+	 * @pre | getLength()>0
+	 * @mutates | this
+	 * @post | getLength() == old(getLength())-1
+	 * @post | IntStream.range(0,old(getArray()).clone().length-1).allMatch(i->getArray()[i]==old(getArray().clone()[i]))
+	 * 
+	 */
+	public void removeLast() {
+		int lengte = getArray().length -1;
+		int[] verkort = new int[lengte];
+		for(int i=0;i<lengte;i++) {
+			verkort[i] = getArray()[i];
+		}
+		this.lijst = verkort;
+		
+	}
+}
---
+++ oefeningintlist/src/IntListTest.java
@@ -0,0 +1,23 @@
+package intlist;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntListTest {
+
+	@Test
+	void test() {
+		int[] argument = {1,2,3,4};
+		IntList myarray = new IntList(argument);
+		assertEquals(4,myarray.getLength());
+		assertEquals(2,myarray.getElementIndex(1));
+		int[] gewijzigde_argument = {1,2,3,4,5};
+		assertArrayEquals(gewijzigde_argument,myarray.addLast(5));
+		int[] gewijzigde_argument_2 = {1,2,3};
+		assertArrayEquals(gewijzigde_argument_2,myarray.removeLast());
+	}
+
+}
